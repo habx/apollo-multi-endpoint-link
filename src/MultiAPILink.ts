ec59e6fe7eb31eaf15716379cf5db6c7f7ffef06
Jean Dessane <jean@habx.fr>
2021-06-17 22:50:18 +0200

    move interface in file

--- src/MultiAPILink.ts
+++ src/MultiAPILink.ts
@@ -10,36 +10,20 @@ import {
   removeDirectivesFromDocument,
 } from '@apollo/client/utilities'
 
+import { MultiAPILinkConfig } from './interface'
 import {
   addApiNameToTypeName,
   getDirectiveArgumentValueFromOperation,
   isFunction,
 } from './utils'
 
-type Config = {
-  endpoints: Record<string, string>
-  createHttpLink: () => ApolloLink
-  createWsLink?: (endpoint: string) => ApolloLink
-  wsSuffix?: string
-  httpSuffix?: string
-  getContext?: (
-    endpoints: string,
-    getCurrentContext: () => Record<string, any>
-  ) => Record<string, any>
-  /**
-   * Add apiName passed in `api` directive to every `__typename` contained in network data response
-   * eg: with `@api(name: 'v1')` directive in your query, an initial typename `Project` would become `v1:Project`
-   */
-  addApiInTypeName?: boolean
-}
-
 export class MultiAPILink extends ApolloLink {
   httpLink: ApolloLink
   wsLinks: Record<string, ApolloLink>
 
-  config: Config
+  config: MultiAPILinkConfig
 
-  constructor(config: Config, request?: RequestHandler) {
+  constructor(config: MultiAPILinkConfig, request?: RequestHandler) {
     super(request)
     this.config = config
     this.httpLink = config.createHttpLink()
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { MultiAPILink } from './MultiAPILink'
+export { MultiAPILinkConfig } from './interface'

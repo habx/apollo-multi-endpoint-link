3e3df234ad0393e3b06e1e9ed7a0b064918fb68a
Jean Dessane <jean@habx.fr>
2021-09-07 10:21:12 +0200

    rollback jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  ...require('@habx/config-ci-front/jest/config'),
-  "testPathIgnorePatterns": [
-    "/node_modules/",
-    "sharedHttpTests.ts"
-  ]
-}
+module.exports = require('@habx/config-ci-front/jest/config')
